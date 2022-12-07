54e6a092e9816411540245511bebd94310e940b0
Brent Crossman <brent.crossman@gmail.com>
2022-12-07 12:31:26 -0500

    day_7 complete

--- Day_7_Part_1.r
+++ Day_7_Part_1.r
@@ -1,3 +1,4 @@
+library(unglue)
 library(tidyverse)
 
 file <- "./Day_7/Part_1/input.txt"
@@ -8,7 +9,31 @@ listing <- unglue_data(input,
                          "{size=\\d+} {file_name}",
                          "dir {dir_contained}"),
                        convert = TRUE)%>% 
-  fill(dir_in, .direction = "down")
+  mutate(dir_in_chg=!is.na(dir_in)) %>% 
+  fill(dir_in, .direction = "down") %>% 
+  mutate(current_dir = "UNKNOWN")
+
+for(i in 1:nrow(listing)){
+  # i = 9
+  if(i ==1){
+    listing$current_dir[i] <- listing$dir_in[i]
+    next
+  }
+  if(listing$dir_in[i] ==  "..") {
+    listing$current_dir[i] <- sub("_[^_]+$", replacement = "", x = listing$current_dir[i-1])  
+    next}
+  if(!listing$dir_in_chg[i]){listing$current_dir[i] <- listing$current_dir[i-1]
+     next
+  }
+  listing$current_dir[i] <- paste(listing$current_dir[i-1], listing$dir_in[i], sep = "_")
+}
+
+listing <- 
+  listing %>% 
+  mutate(dir_in = current_dir) %>% 
+  mutate(dir_contained = ifelse(!is.na(dir_contained), paste(dir_in, dir_contained, sep = "_"), dir_contained)) %>% 
+  select(dir_in:file_name)
+
 
 directory_mapping <- 
   listing %>% 
@@ -38,7 +63,7 @@ current_size_folders <- size_folder_excl_sub_full
 count <- 0
 while(nrow(remaining_folders)>0){
   print(nrow(remaining_folders))
-  if(nrow(remaining_folders)==101){break}
+  # if(nrow(remaining_folders)==101){break}
   count <- count+1
   
   active_folders <- 
@@ -73,9 +98,22 @@ while(nrow(remaining_folders)>0){
       select(dir_in,dir_contained)
 } 
 
+full_run <- 
 current_size_folders %>% 
   rename(total_size = size) %>% 
-  bind_rows(bind_rows(output_list)) %>% 
+  bind_rows(bind_rows(output_list)) 
+
+##Part 1
+full_run %>% 
   filter(total_size<=100000) %>% 
   pull(total_size) %>% 
   sum()
+
+##Part 2
+space_needed = 30000000-(70000000-max(full_run$total_size))
+
+full_run %>% 
+  ungroup() %>% 
+  filter(total_size>space_needed) %>% 
+  slice_min(total_size, n = 1) %>% 
+  pull(total_size)
--- Day_7_Part_1_rec.r
+++ Day_7_Part_1_rec.r
@@ -1,3 +1,4 @@
+library(unglue)
 library(tidyverse)
 
 file <- "./Day_7/Part_1/input.txt"
@@ -8,7 +9,31 @@ listing <- unglue_data(input,
                          "{size=\\d+} {file_name}",
                          "dir {dir_contained}"),
                        convert = TRUE)%>% 
-  fill(dir_in, .direction = "down")
+  mutate(dir_in_chg=!is.na(dir_in)) %>% 
+  fill(dir_in, .direction = "down") %>% 
+  mutate(current_dir = "UNKNOWN")
+
+for(i in 1:nrow(listing)){
+  # i = 9
+  if(i ==1){
+    listing$current_dir[i] <- listing$dir_in[i]
+    next
+  }
+  if(listing$dir_in[i] ==  "..") {
+    listing$current_dir[i] <- sub("_[^_]+$", replacement = "", x = listing$current_dir[i-1])  
+    next}
+  if(!listing$dir_in_chg[i]){listing$current_dir[i] <- listing$current_dir[i-1]
+  next
+  }
+  listing$current_dir[i] <- paste(listing$current_dir[i-1], listing$dir_in[i], sep = "_")
+}
+
+listing <- 
+  listing %>% 
+  mutate(dir_in = current_dir) %>% 
+  mutate(dir_contained = ifelse(!is.na(dir_contained), paste(dir_in, dir_contained, sep = "_"), dir_contained)) %>% 
+  select(dir_in:file_name)
+
 
 directory_mapping <- 
   listing %>% 
@@ -51,15 +76,34 @@ total_folder_size <- function(dir_target){
     for(i in sub_dir){
       addl_size <- addl_size+total_folder_size(i)
     } 
-      folder_alone <- 
-        size_folder_excl_sub_full %>% 
-        filter(dir_in==dir_target) %>% 
-        pull(size) %>% 
-        sum()
-      
-      return(addl_size+folder_alone)
+    folder_alone <- 
+      size_folder_excl_sub_full %>% 
+      filter(dir_in==dir_target) %>% 
+      pull(size) %>% 
+      sum()
+    
+    return(addl_size+folder_alone)
   }
 }
+# 
+# total_folder_size("/")
 
-total_folder_size("/")
-   
+full_run <- 
+  size_folder_excl_sub_full %>% 
+  rowwise() %>% 
+  mutate(total_size = total_folder_size(dir_in))
+
+##Part 1
+full_run %>% 
+  filter(total_size<=100000) %>% 
+  pull(total_size) %>% 
+  sum()
+
+##Part 2
+space_needed = 30000000-(70000000-max(full_run$total_size))
+
+full_run %>% 
+  ungroup() %>% 
+  filter(total_size>space_needed) %>% 
+  slice_min(total_size, n = 1) %>% 
+  pull(total_size)
