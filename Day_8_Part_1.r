1dd3767e1e621e5fc7c93a1e19f884e89739bb4c
Brent Crossman <brent.crossman@gmail.com>
2022-12-08 11:02:40 -0500

    update_day_8

--- Day_8_Part_1.r
+++ Day_8_Part_1.r
@@ -43,7 +43,7 @@ data %>%
 ## Part 2
 
 left_count <- function(i){
-  # i <- data$key[1]
+  # i <- "4,3"
   
   vec <- 
     data %>% 
@@ -56,14 +56,14 @@ left_count <- function(i){
     
     return(
       vec %>% 
+        arrange(colid) %>% 
         mutate(can_see_over = (value-data$value[data$key==i])<0) %>%
         mutate(group = cumsum(can_see_over!=lag(can_see_over,1, TRUE))) %>% 
-        group_by(group, can_see_over) %>% 
-        mutate(count = seq(n())) %>% 
-        ungroup() %>% 
+        mutate(starting_group = min(group)) %>% 
         slice_max(group) %>% 
+        mutate(can_see_over = ifelse((group==min(starting_group))&(row_number()==min(row_number())),FALSE,can_see_over)) %>% 
         pull(can_see_over) %>% 
-        sum()
+        sum()+1
     )
   }
 }
@@ -79,44 +79,20 @@ right_count <- function(i){
         
         return(
           vec %>% 
+            arrange(desc(colid)) %>% 
             mutate(can_see_over = (value-data$value[data$key==i])<0) %>%
             mutate(group = cumsum(can_see_over!=lag(can_see_over,1, TRUE))) %>% 
-            group_by(group, can_see_over) %>% 
-            mutate(count = seq(n())) %>% 
-            ungroup() %>% 
+            mutate(starting_group = min(group)) %>% 
             slice_max(group) %>% 
+            mutate(can_see_over = ifelse((group==min(starting_group))&(row_number()==min(row_number())),FALSE,can_see_over)) %>% 
             pull(can_see_over) %>% 
-            sum()
+            sum()+1
         )
       }
 }
 down_count <- function(i){
   vec <- 
     data %>%
-    filter(rowid < data$rowid[data$key==i],
-           colid == data$colid[data$key==i]) 
-  
-  if(nrow(vec)==0) {
-    return(0)}else{
-      
-      return(
-        vec %>% 
-          mutate(can_see_over = (value-data$value[data$key==i])<0) %>%
-          mutate(group = cumsum(can_see_over!=lag(can_see_over,1, TRUE))) %>% 
-          group_by(group, can_see_over) %>% 
-          mutate(count = seq(n())) %>% 
-          ungroup() %>% 
-          slice_max(group) %>% 
-          pull(can_see_over) %>% 
-          sum()
-      )
-    }
-}
-
-up_count <- function(i){
-  
-  vec <- 
-    data %>% 
     filter(rowid > data$rowid[data$key==i],
            colid == data$colid[data$key==i]) 
   
@@ -125,14 +101,38 @@ up_count <- function(i){
       
       return(
         vec %>% 
+          arrange(desc(rowid)) %>% 
           mutate(can_see_over = (value-data$value[data$key==i])<0) %>%
           mutate(group = cumsum(can_see_over!=lag(can_see_over,1, TRUE))) %>% 
-          group_by(group, can_see_over) %>% 
-          mutate(count = seq(n())) %>% 
-          ungroup() %>% 
+          mutate(starting_group = min(group)) %>% 
           slice_max(group) %>% 
+          mutate(can_see_over = ifelse((group==min(starting_group))&(row_number()==min(row_number())),FALSE,can_see_over)) %>% 
           pull(can_see_over) %>% 
-          sum()
+          sum()+1
+      )
+    }
+}
+
+up_count <- function(i){
+  
+  vec <- 
+    data %>% 
+    filter(rowid < data$rowid[data$key==i],
+           colid == data$colid[data$key==i]) 
+  
+  if(nrow(vec)==0) {
+    return(0)}else{
+      
+      return(
+        vec %>% 
+          arrange((rowid)) %>% 
+          mutate(can_see_over = (value-data$value[data$key==i])<0) %>%
+          mutate(group = cumsum(can_see_over!=lag(can_see_over,1, TRUE))) %>% 
+          mutate(starting_group = min(group)) %>% 
+          slice_max(group) %>% 
+          mutate(can_see_over = ifelse((group==min(starting_group))&(row_number()==min(row_number())),FALSE,can_see_over)) %>% 
+          pull(can_see_over) %>% 
+          sum()+1
       )
     }
 }
@@ -141,19 +141,16 @@ data$right <- 0
 data$up <- 0
 data$down <- 0
 
-# for(i in 1:nrow(data)){
-#   # i <- 1
-#   print(data$key[i])
-#   data$left[i] <- left_count(data$key[i])
-#   data$right[i] <- right_count(data$key[i])
-#   data$up[i] <- up_count(data$key[i])
-#   data$down[i] <- down_count(data$key[i])
-# }
+for(i in 1:nrow(data)){
+  # i <- 18
+  print(data$key[i])
+  data$left[i] <- left_count(data$key[i])
+  data$right[i] <- right_count(data$key[i])
+  data$up[i] <- up_count(data$key[i])
+  data$down[i] <- down_count(data$key[i])
+}
 
-result <-
-  data %>%
-  rowwise() %>%
-  mutate(left = left_count(key),
-         right = right_count(key),
-         up = up_count(key),
-         down = down_count(key))
+data %>% 
+  mutate(total_view = left*right*up*down) %>% 
+  pull(total_view) %>% 
+  max()
