891a4dc96db86d9c31c54e05a8a9e11c4e0cdfea
Brent Crossman <brent.crossman@gmail.com>
2022-12-09 13:41:55 -0500

    done

--- Day_9_Part_2.r
+++ Day_9_Part_2.r
@@ -20,24 +20,29 @@ for(i in 0:number_of_knots){
 update_tail <- function(h_x, h_y, t_x, t_y){
   x_dist <- (h_x)-(t_x)
   y_dist <- (h_y)-(t_y)
-  
-  if(abs(x_dist)>1){
-    if(sqrt(x_dist^2+y_dist^2)>1){
-      t_y <- t_y+y_dist
-    }
+  if((abs(x_dist)>1)&(abs(y_dist)>1)){
     t_x <- t_x+x_dist/2
-  }
-  if(abs(y_dist)>1){
-    if(sqrt(x_dist^2+y_dist^2)>1){
-      t_x <- t_x+x_dist
-    }
     t_y <- t_y+y_dist/2
   }
+  if((abs(x_dist)>1)&(abs(y_dist)==1)){
+    t_x <- t_x+x_dist/2
+    t_y <- t_y+y_dist
+  }
+  if((abs(x_dist)>1)&(abs(y_dist)==0)){
+    t_x <- t_x+x_dist/2
+  }
+  if((abs(x_dist)==1)&(abs(y_dist)>1)){
+    t_y <- t_y+y_dist/2
+    t_x <- t_x+x_dist
+  }
+  if((abs(x_dist)==0)&(abs(y_dist)>1)){
+    t_y <- t_y+y_dist/2
+  }
+  
   return(list(t_x,t_y))
 }
 
-output <- list()
-count <- 0
+output <- data.frame(x=c(), y=c())
 for(i in 1:nrow(moves)){
   print(i)
   if(moves$move[i]%in% c("L", "R")){
@@ -54,9 +59,8 @@ for(i in 1:nrow(moves)){
                                 t_y = knot_positions[[as.character(k)]]$y)
         knot_positions[[as.character(k)]] <- data.frame(x =new_tail[[1]], 
                                                         y = new_tail[[2]])
-        output[[as.character(count)]] <- knot_positions[[as.character(number_of_knots)]]
-        count <- count+1
       }
+      output <- bind_rows(output,knot_positions[[as.character(number_of_knots)]]) %>% distinct_all()
     }
   }
   if(moves$move[i]%in% c("U", "D")){
@@ -66,21 +70,20 @@ for(i in 1:nrow(moves)){
       curr_y = knot_positions[["0"]]$y
       knot_positions[["0"]] = data.frame(x = curr_x, y = curr_y+y_move)
       for(k in 1:number_of_knots){
-        # k <- 1
+        # k <- 3
+        # if(i==2&j==4&k==6){asdfasd}
         new_tail <- update_tail(h_x = knot_positions[[as.character(k-1)]]$x,
                                 h_y = knot_positions[[as.character(k-1)]]$y,
                                 t_x = knot_positions[[as.character(k)]]$x,
                                 t_y = knot_positions[[as.character(k)]]$y)
         knot_positions[[as.character(k)]] <- data.frame(x =new_tail[[1]], 
                                                         y = new_tail[[2]])
-        output[[as.character(count)]] <- knot_positions[[as.character(number_of_knots)]]
-        count <- count+1
       }
+      output <- bind_rows(output,knot_positions[[as.character(number_of_knots)]]) %>%  distinct_all()
     }
   }
 }
 
-  bind_rows(output) %>% 
-    distinct_all() %>% 
-    nrow()
-  
+output %>% 
+  distinct_all() %>% 
+  nrow()
